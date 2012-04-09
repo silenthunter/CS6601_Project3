2049c910b54ddc50e37ee60a6b7376a3ed690d68
silenthunter <silenthunter@gmail.com>
2012-04-09 18:59:16 -0400

    The program will now convert the loaded image into CIELUV format and save it

--- Project3.cpp
+++ Project3.cpp
@@ -5,9 +5,18 @@
 int main(int argc, char* argv[])
 {
 	IplImage* img = cvLoadImage(argv[1]);
+	IplImage* luv;
 	if(!img)
 	{
 		printf("Could not load %s\n", argv[1]);
 		exit(0);
 	}
+
+	printf("Width: %d\nHeight: %d\n", img->width, img->height);
+
+	//Set luv to the be same size as img
+	luv = cvCreateImage(cvGetSize(img), img->depth, img->nChannels);
+
+	cvCvtColor(img, luv, CV_RGB2Luv);
+	cvSaveImage("CIELUV.jpg", luv);
 }
